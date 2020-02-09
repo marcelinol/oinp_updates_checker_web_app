6180b576a7fe19547bf82a11c58deff8b2ac18a3
Luciano Medeiros Marcelino <lucianom@nulogy.com>
2020-02-08 22:25:13 -0500

    Run tests on every commit

--- aws_initializer.rb
+++ aws_initializer.rb
@@ -2,7 +2,11 @@ require "aws-sdk-s3"
 require "dotenv"
 Dotenv.load("#{__dir__}/.env")
 
+region = ENV["AWS_REGION"] || "us-east-1"
+access_key = ENV["AWS_ACCESS_KEY_ID"] || "none"
+secret_key = ENV["AWS_SECRET_ACCESS_KEY"] || "secret"
+
 Aws.config.update(
-  region: ENV["AWS_REGION"],
-  credentials: Aws::Credentials.new(ENV["AWS_ACCESS_KEY_ID"], ENV["AWS_SECRET_ACCESS_KEY"])
+  region: region,
+  credentials: Aws::Credentials.new(access_key, secret_key)
 )
