940ca1c4e3328cabc81f8967b9e159a2b092f588
Luciano Medeiros Marcelino <lucianom@nulogy.com>
2020-02-09 16:52:58 -0500

    Show feedback of form submit to the user

--- app.rb
+++ app.rb
@@ -3,14 +3,25 @@ require "sinatra/reloader" if development?
 require_relative "lib/user_subscriber"
 require_relative "lib/logs_fetcher"
 
+enable :sessions
+
 get "/" do
+  @message = session.delete(:message)
   @logs = LogsFetcher.new.logs
   erb :"index.html"
 end
 
 post "/register" do
   UserSubscriber.new(params[:email]).subscribe!
-  "SUCCESS"
-rescue StandardError => e
-  e.message
+  session[:message] = {
+    status: "success",
+    content: "Email #{params[:email]} successfully registered."
+  }
+  redirect("/")
+rescue StandardError, ArgumentError => e
+  session[:message] = {
+    status: "failure",
+    content: e.message
+  }
+  redirect("/")
 end
